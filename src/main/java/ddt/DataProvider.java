802886733922bc420ee1610fd9d2bc4a463e7ff8
youvegotnigel <nigeldave19@gmail.com>
2020-09-20 14:42:14 +0530

    small changes

--- src/main/java/ddt/DataProvider.java
+++ src/main/java/ddt/DataProvider.java
@@ -7,10 +7,8 @@ public class DataProvider {
         return new Object[][]{
                 {"ant-aal-aepz", "ant-aal-aepz", "8_5shift_D_2020-09-20"},
                 {"ant-aal-aepz", "ant-aal-aepz", "8_5shift_D_2020-09-19"},
-                {"ant-aal-aepz", "ant-aal-aepz", "8_5shift_D_2020-09-18"},
                 {"ugl-ftml-unit1", "ugl-ftml-unit1", "day_shift_D_2020-09-20"},
                 {"ugl-ftml-unit1", "ugl-ftml-unit1", "day_shift_D_2020-09-19"},
-                {"ugl-ftml-unit1", "ugl-ftml-unit1", "day_shift_D_2020-09-18"},
                 {"bhg-knit", "bhg-knit", "day_shift_D_2020-09-20"},
                 {"bhg-knit", "bhg-knit", "day_shift_D_2020-09-19"},
                 {"bhg-knit", "bhg-knit", "day_shift_D_2020-09-18"},
