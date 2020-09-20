f62d95aa79584a59f6281d9a1b6959f9e7ec11b9
youvegotnigel <nigeldave19@gmail.com>
2020-09-20 14:18:59 +0530

    add new data to data provider

--- src/main/java/ddt/DataProvider.java
+++ src/main/java/ddt/DataProvider.java
@@ -5,12 +5,16 @@ public class DataProvider {
     @org.testng.annotations.DataProvider(name = "dataForPost")
     public static Object[][] dataForPost() {
         return new Object[][]{
+                {"ant-aal-aepz", "ant-aal-aepz", "8_5shift_D_2020-09-20"},
                 {"ant-aal-aepz", "ant-aal-aepz", "8_5shift_D_2020-09-19"},
                 {"ant-aal-aepz", "ant-aal-aepz", "8_5shift_D_2020-09-18"},
+                {"ugl-ftml-unit1", "ugl-ftml-unit1", "day_shift_D_2020-09-20"},
                 {"ugl-ftml-unit1", "ugl-ftml-unit1", "day_shift_D_2020-09-19"},
                 {"ugl-ftml-unit1", "ugl-ftml-unit1", "day_shift_D_2020-09-18"},
+                {"bhg-knit", "bhg-knit", "day_shift_D_2020-09-20"},
                 {"bhg-knit", "bhg-knit", "day_shift_D_2020-09-18"},
                 {"bhg-knit", "bhg-knit", "day_shift_D_2020-09-18"},
+                {"pds-paba", "pds-paba", "day_shift_D_2020-09-20"},
                 {"pds-paba", "pds-paba", "day_shift_D_2020-09-18"},
                 {"pds-paba", "pds-paba", "day_shift_D_2020-09-18"}
         };
